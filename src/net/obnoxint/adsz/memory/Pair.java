cbc4de09e2e945e621b31a753bcfc056dff30fe9
obnoxint <mail@obnoxint.net>
2012-11-23 08:53:24 +0100

    Moved constant CARD_SIZE to Main and renamed it to GAME_CARD_SIZE.

--- src/net/obnoxint/adsz/memory/Pair.java
+++ src/net/obnoxint/adsz/memory/Pair.java
@@ -4,16 +4,14 @@ import org.lwjgl.util.Point;
 
 class Pair {
 
-    static final int CARD_SIZE = 96; // size per card in px
-
     final Box[] cards = new Box[2];
     final int id;
 
     boolean[] uncovered = { false, false };
 
     Pair(final Point first, final Point second, final int id) {
-        this.cards[0] = new Box(new Point(first), new Point(first.getX() + CARD_SIZE, first.getY() + CARD_SIZE));
-        this.cards[1] = new Box(new Point(second), new Point(second.getX() + CARD_SIZE, second.getY() + CARD_SIZE));
+        this.cards[0] = new Box(new Point(first), new Point(first.getX() + Main.GAME_CARD_SIZE, first.getY() + Main.GAME_CARD_SIZE));
+        this.cards[1] = new Box(new Point(second), new Point(second.getX() + Main.GAME_CARD_SIZE, second.getY() + Main.GAME_CARD_SIZE));
         this.id = id;
     }
 
