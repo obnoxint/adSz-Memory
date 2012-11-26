31d8292713d38e3638dba85b33640d38bba017d6
obnoxint <mail@obnoxint.net>
2012-11-26 10:51:00 +0100

    Added State.java and StatePairSelection.java.

---
+++ src/net/obnoxint/adsz/memory/State.java
@@ -0,0 +1,111 @@
+package net.obnoxint.adsz.memory;
+
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glTexCoord2f;
+import static org.lwjgl.opengl.GL11.glVertex2i;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import org.lwjgl.util.Point;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+
+abstract class State {
+
+    private static final String FILE_BG_PREFIX = "bg_";
+    private static final Box BACKGROUND = new Box(new Point(0, 0), new Point(Main.DISPLAY_WIDTH, Main.DISPLAY_HEIGHT));
+
+    private static Map<Integer, State> states = new HashMap<>();
+    private static State activeState = null;
+
+    static final int STATE_INTRO = 0;
+    static final int STATE_PAIRSELECTION = 1;
+    static final int STATE_PLAY = 2;
+    static final int STATE_END = 3;
+
+    protected static State getState(final int id) {
+        State r = null;
+        if (states.containsKey(id)) {
+            r = states.get(id);
+        } else {
+            try {
+                switch (id) {
+                case STATE_INTRO:
+                    r = new StateIntro();
+                break;
+                case STATE_PAIRSELECTION:
+                    r = new StatePairSelection();
+                break;
+                case STATE_PLAY:
+                    r = new StatePlay();
+                break;
+                case STATE_END:
+                    r = new StateEnd();
+                break;
+                }
+                if (r != null) {
+                    states.put(r.id, r);
+                }
+            } catch (final IOException e) {
+                JOptionPane.showMessageDialog(null, "Beim initialisieren des Spielzustands " + id + " ist ein Problem aufgetreten: " + e.getLocalizedMessage(), "Fehler", JOptionPane.ERROR_MESSAGE);
+                Main.writeStackTrace(e);
+                System.exit(Main.EXIT_CODE_ERROR);
+            }
+        }
+        return r;
+    }
+
+    static State getActiveState() {
+        return activeState;
+    }
+
+    static void setActiveState(final int id) {
+        activeState = getState(id);
+        if (id == STATE_PLAY) {
+            ((StatePlay) activeState).init();
+        }
+    }
+
+    final int id;
+    final Texture bg;
+
+    protected State(final int id) throws IOException {
+        this.id = id;
+        try (FileInputStream fis = new FileInputStream(new File(Main.instance.getRessourceFolder(), FILE_BG_PREFIX + id + Main.FILE_EXT_PNG))) {
+            this.bg = TextureLoader.getTexture(Main.TEXTURE_TYPE_PNG, fis);
+        }
+    }
+
+    abstract void draw();
+
+    final void drawBackground() {
+        drawTexturedBox(BACKGROUND, bg);
+    }
+
+    void drawTexturedBox(final Box box, final Texture texture) {
+        texture.bind();
+        glBegin(GL_QUADS);
+        {
+            glTexCoord2f(0f, 0f);
+            glVertex2i(box.getUpperLeft().getX(), box.getUpperLeft().getY());
+            glTexCoord2f(texture.getWidth(), 0f);
+            glVertex2i(box.getUpperLeft().getX() + box.getWidth(), box.getUpperLeft().getY());
+            glTexCoord2f(texture.getWidth(), texture.getHeight());
+            glVertex2i(box.getUpperLeft().getX() + box.getWidth(), box.getUpperLeft().getY() + box.getHeight());
+            glTexCoord2f(0f, texture.getHeight());
+            glVertex2i(box.getUpperLeft().getX(), box.getUpperLeft().getY() + box.getHeight());
+        }
+        glEnd();
+    }
+
+    abstract void handleInput();
+
+}
---
+++ src/net/obnoxint/adsz/memory/StatePairSelection.java
@@ -0,0 +1,107 @@
+package net.obnoxint.adsz.memory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import org.lwjgl.util.Point;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+
+class StatePairSelection extends State {
+
+    private static final String RES_BUTTON_NEXT = "next" + Main.FILE_EXT_PNG;
+    private static final String RES_BUTTON_PREVIOUS = "prev" + Main.FILE_EXT_PNG;
+    private static final String RES_BUTTON_PLAY = "play" + Main.FILE_EXT_PNG;
+    private static final String RES_HOVER_PREFIX = "h_";
+
+    private static final Box BUTTON_NEXT = new Box(new Point(250, 325), new Point(300, 350));
+    private static final Box BUTTON_PREVIOUS = new Box(new Point(250, 425), new Point(300, 450));
+    private static final Box BUTTON_PLAY = new Box(new Point(400, 375), new Point(600, 425));
+    private static final Point SELECTION_POS = new Point(190, 360);
+
+    private boolean switchCooldown = false;
+
+    Difficulty difficulty = Difficulty._3;
+
+    private final Map<String, Texture> textures = new HashMap<>();
+
+    StatePairSelection() throws IOException {
+        super(State.STATE_PAIRSELECTION);
+    }
+
+    private void changeDifficulty(final boolean next) {
+        if (!switchCooldown) {
+            difficulty = (next) ? Difficulty.getNext(difficulty) : Difficulty.getPrevious(difficulty);
+            new Thread() {
+
+                @Override
+                public void run() {
+                    try {
+                        switchCooldown = true;
+                        sleep(200);
+                    } catch (final InterruptedException e) {} finally {
+                        switchCooldown = false;
+                    }
+                }
+
+            }.start();
+        }
+    }
+
+    private void drawBox(final Box box, final String id, final boolean hover) {
+        drawTexturedBox(box, getTexture(id, hover));
+
+    }
+
+    private void drawSelection() {
+        Main.instance.font.drawString(SELECTION_POS.getX(), SELECTION_POS.getY(), difficulty.count() / 2 + " Paare");
+
+    }
+
+    private Texture getTexture(final String id, final boolean hover) {
+        final String n = (hover) ? RES_HOVER_PREFIX + id : id;
+        Texture r = textures.get(n);
+        if (r == null) {
+            try (FileInputStream fis = new FileInputStream(new File(Main.instance.getRessourceFolder(), n))) {
+                r = TextureLoader.getTexture(Main.TEXTURE_TYPE_PNG, fis);
+                textures.put(n, r);
+            } catch (final IOException e) {
+                JOptionPane.showMessageDialog(null, "Textur konnte nicht geladen werden: " + n, "Fehler", JOptionPane.ERROR_MESSAGE);
+                Main.writeStackTrace(e);
+                System.exit(Main.EXIT_CODE_ERROR);
+            }
+        }
+        return r;
+    }
+
+    private boolean isHover(final Box box) {
+        return box.isInside(Main.instance.mouse_abs_x, Main.instance.mouse_abs_y);
+    }
+
+    @Override
+    void draw() {
+        drawBox(BUTTON_NEXT, RES_BUTTON_NEXT, isHover(BUTTON_NEXT));
+        drawBox(BUTTON_PREVIOUS, RES_BUTTON_PREVIOUS, isHover(BUTTON_PREVIOUS));
+        drawBox(BUTTON_PLAY, RES_BUTTON_PLAY, isHover(BUTTON_PLAY));
+        drawSelection();
+    }
+
+    @Override
+    void handleInput() {
+        if (Main.instance.mouse_but_l) {
+            if (isHover(BUTTON_NEXT)) {
+                changeDifficulty(true);
+            } else if (isHover(BUTTON_PREVIOUS)) {
+                changeDifficulty(false);
+            } else if (isHover(BUTTON_PLAY)) {
+                setActiveState(STATE_PLAY);
+            }
+        }
+    }
+
+}
