fd553bca2911809bf1d6704ee98ef8418e396de7
obnoxint <mail@obnoxint.net>
2012-12-04 08:49:48 +0100

    Added game summary.

--- src/net/obnoxint/adsz/memory/StatePlay.java
+++ src/net/obnoxint/adsz/memory/StatePlay.java
@@ -18,10 +18,31 @@ class StatePlay extends State {
     private static final Box BUTTON_BACK = new Box(new Point(945, 360), new Point(1009, 424));
     private static final Box BUTTON_RESTART = new Box(new Point(945, 560), new Point(1009, 624));
 
+    private static final Point TEXT_SCORE = new Point(100, 250);
+
     private MemoryGame game = null;
+    private int[] summary;
 
     StatePlay() throws IOException {
         super(State.STATE_PLAY);
+    }
+
+    private void createSummary() {
+        summary = new int[2];
+
+        final int d = ((StatePairSelection) getState(STATE_PAIRSELECTION)).difficulty.pairs() * 100;
+        final int t = Math.round((game.finished - game.started) / 1000);
+        final int m = game.mistakes * 10;
+        int s = d - t - m;
+
+        if (s < 0) {
+            s = 0;
+        }
+
+        final int p = Math.round((100f / d) * s);
+
+        summary[0] = s;
+        summary[1] = p;
     }
 
     private void drawButtons() {
@@ -31,7 +52,29 @@ class StatePlay extends State {
     }
 
     private void drawSummary() {
-        // TODO Auto-generated method stub
+        if (summary == null) {
+            createSummary();
+        }
+        final int s = summary[0];
+        final int p = summary[1];
+
+        String c;
+        if (p >= 90) {
+            c = "Wow!\nSehr beeindruckend!";
+        } else if (p >= 80) {
+            c = "Hervorragend!";
+        } else if (p >= 60) {
+            c = "Sehr gut!";
+        } else if (p >= 40) {
+            c = "Gut!\nAber das kannst Du besser!";
+        } else if (p >= 20) {
+            c = "Übe weiter!\nDas kannst Du besser!";
+        } else {
+            c = "Schade!\nGib nicht auf und versuche es nochmal!";
+        }
+
+        Main.instance.font.drawString(TEXT_SCORE.getX(), TEXT_SCORE.getY(), "Du hast " + s + " Punkte erreicht (" + p + "%).");
+        Main.instance.font.drawString(TEXT_SCORE.getX() + 50, TEXT_SCORE.getY() + 100, c);
     }
 
     @Override
@@ -71,6 +114,7 @@ class StatePlay extends State {
 
     void init() {
         game = new MemoryGame(((StatePairSelection) State.getState(STATE_PAIRSELECTION)).difficulty);
+        summary = null;
     }
 
 }
